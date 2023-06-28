2308955e88e7c11f6f1e11395de68e08f8a52774
Jay-ar Collantes <sweetinvader404@gmail.com>
2023-06-28 16:31:06 +0800

    Facebook Live chat update

--- assets/js/script.js
+++ assets/js/script.js
@@ -51,21 +51,21 @@ window.fbAsyncInit = function () {
 })(document, "script", "facebook-jssdk");
 
 /****************end Official FACEBOOK SDK****************** */
-function chatboxInit() {
-  window.fbAsyncInit = function () {
-    FB.init({
-      xfbml: true,
-      version: "v14.0"
-    });
-  };
-  (function (d, s, id) {
-    var js,
-      fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) return;
-    js = d.createElement(s);
-    js.id = id;
-    js.src = "https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js";
-    fjs.parentNode.insertBefore(js, fjs);
-  })(document, "script", "facebook-jssdk");
-}
-chatboxInit();
+// function chatboxInit() {
+//   window.fbAsyncInit = function () {
+//     FB.init({
+//       xfbml: true,
+//       version: "v14.0"
+//     });
+//   };
+//   (function (d, s, id) {
+//     var js,
+//       fjs = d.getElementsByTagName(s)[0];
+//     if (d.getElementById(id)) return;
+//     js = d.createElement(s);
+//     js.id = id;
+//     js.src = "https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js";
+//     fjs.parentNode.insertBefore(js, fjs);
+//   })(document, "script", "facebook-jssdk");
+// }
+// chatboxInit();
