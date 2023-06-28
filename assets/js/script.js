d8b5bb2588eeaf62bd194eb0137ab155c2d27281
Jay-ar Collantes <sweetinvader404@gmail.com>
2023-06-28 16:28:47 +0800

    Facebook live chat update

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,6 +30,27 @@ sections.forEach(e => {
   updateNavigationStyle();
 
 /***************************Facebook live chat************ */
+/****************Official FACEBOOK SDK****************** */
+window.fbAsyncInit = function () {
+  FB.init({
+    appId: "{113810161758805}",
+    autoLogAppEvents: true,
+    xfbml: true,
+    version: "v14.0"
+  });
+};
+
+(function (d, s, id) {
+  var js,
+    fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s);
+  js.id = id;
+  js.src = "https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js";
+  fjs.parentNode.insertBefore(js, fjs);
+})(document, "script", "facebook-jssdk");
+
+/****************end Official FACEBOOK SDK****************** */
 function chatboxInit() {
   window.fbAsyncInit = function () {
     FB.init({
