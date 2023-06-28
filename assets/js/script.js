f2938ddeda4c8a2f5801ef75332f50139da12f4c
Jay-ar Collantes <sweetinvader404@gmail.com>
2023-06-28 16:20:27 +0800

    Facebook live chat change

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,50 @@
-function updateActiveLink(e){e.forEach(e=>{let t=e.target.getAttribute("id"),a=document.querySelector(`.navbar a[href="#${t}"]`);a&&(e.isIntersecting?a.classList.add("active"):a.classList.remove("active"))})}document.addEventListener("DOMContentLoaded",function(){let e=document.querySelector(".rounded-image"),t=e.dataset.src,a=new Image;a.src=t,a.onload=function(){e.src=t}});const observer=new IntersectionObserver(updateActiveLink,{threshold:.5}),sections=document.querySelectorAll("section");function updateNavigationStyle(){var e=document.querySelector(".header");window.scrollY>0?e.classList.add("active"):e.classList.remove("active")}sections.forEach(e=>{observer.observe(e)}),window.addEventListener("scroll",updateNavigationStyle),updateNavigationStyle();
+function updateActiveLink(e) {
+  e.forEach(e => {
+    let t = e.target.getAttribute("id"),
+      a = document.querySelector(`.navbar a[href="#${t}"]`);
+    a &&
+      (e.isIntersecting
+        ? a.classList.add("active")
+        : a.classList.remove("active"));
+  });
+}
+document.addEventListener("DOMContentLoaded", function () {
+  let e = document.querySelector(".rounded-image"),
+    t = e.dataset.src,
+    a = new Image();
+  (a.src = t),
+    (a.onload = function () {
+      e.src = t;
+    });
+});
+const observer = new IntersectionObserver(updateActiveLink, { threshold: 0.5 }),
+  sections = document.querySelectorAll("section");
+function updateNavigationStyle() {
+  var e = document.querySelector(".header");
+  window.scrollY > 0 ? e.classList.add("active") : e.classList.remove("active");
+}
+sections.forEach(e => {
+  observer.observe(e);
+}),
+  window.addEventListener("scroll", updateNavigationStyle),
+  updateNavigationStyle();
+
+/***************************Facebook live chat************ */
+function chatboxInit() {
+  window.fbAsyncInit = function () {
+    FB.init({
+      xfbml: true,
+      version: "v14.0"
+    });
+  };
+  (function (d, s, id) {
+    var js,
+      fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s);
+    js.id = id;
+    js.src = "https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js";
+    fjs.parentNode.insertBefore(js, fjs);
+  })(document, "script", "facebook-jssdk");
+}
+chatboxInit();
