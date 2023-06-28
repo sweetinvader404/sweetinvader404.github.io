1d49971c888e8250a533aaa4e939bd4ef5233a9a
Jay-ar Collantes <sweetinvader404@gmail.com>
2023-06-29 03:27:50 +0800

    lazyload update

--- assets/js/lazyload.js
+++ assets/js/lazyload.js
@@ -56,7 +56,7 @@ const isSectionNearViewportBottom = section => {
   const rect = section.getBoundingClientRect();
   const viewportHeight = window.innerHeight;
 
-  return rect.top <= viewportHeight;
+  return rect.top <= viewportHeight - 60;
 };
 
 // Lazy load JavaScript file
