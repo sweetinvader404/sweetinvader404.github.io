340a90239b26903d133639fca15a6d099709b602
Jay-ar Collantes <sweetinvader404@gmail.com>
2023-06-29 06:25:32 +0800

    udpate

--- assets/js/lazyload.js
+++ assets/js/lazyload.js
@@ -56,7 +56,7 @@ const isSectionNearViewportBottom = section => {
   const rect = section.getBoundingClientRect();
   const viewportHeight = window.innerHeight;
 
-  return rect.top <= viewportHeight + 140;
+  return rect.top <= viewportHeight + 120;
 };
 
 // Lazy load JavaScript file
