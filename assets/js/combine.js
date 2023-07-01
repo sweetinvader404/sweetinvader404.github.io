3406cea7eec44404a2f873569b8f8a1bddfb888e
Jay-ar Collantes <sweetinvader404@gmail.com>
2023-07-02 05:39:21 +0800

    update

--- assets/js/combine.js
+++ assets/js/combine.js
@@ -325,24 +325,24 @@ sections.forEach(e => {
 // window.addEventListener("load", loadFacebookMessengerChat);
 /****************Defer facebook messenger******************** */
 /****************Official FACEBOOK SDK****************** */
-window.fbAsyncInit = function () {
-  FB.init({
-    appId: "591873306363263",
-    autoLogAppEvents: true,
-    xfbml: true,
-    version: "v13.0"
-  });
-};
+// window.fbAsyncInit = function () {
+//   FB.init({
+//     appId: "591873306363263",
+//     autoLogAppEvents: true,
+//     xfbml: true,
+//     version: "v13.0"
+//   });
+// };
 
-(function (d, s, id) {
-  var js,
-    fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s);
-  js.id = id;
-  js.src = "https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js";
-  fjs.parentNode.insertBefore(js, fjs);
-})(document, "script", "facebook-jssdk");
+// (function (d, s, id) {
+//   var js,
+//     fjs = d.getElementsByTagName(s)[0];
+//   if (d.getElementById(id)) return;
+//   js = d.createElement(s);
+//   js.id = id;
+//   js.src = "https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js";
+//   fjs.parentNode.insertBefore(js, fjs);
+// })(document, "script", "facebook-jssdk");
 
 /****************end Official FACEBOOK SDK****************** */
 
