380d641d0437bd2f1c43afa7e5f62577bcc26a8c
Jay-ar Collantes <sweetinvader404@gmail.com>
2023-07-02 04:55:41 +0800

    update

--- assets/js/combine.js
+++ assets/js/combine.js
@@ -325,24 +325,63 @@ sections.forEach(e => {
 // window.addEventListener("load", loadFacebookMessengerChat);
 /****************Defer facebook messenger******************** */
 /****************Official FACEBOOK SDK****************** */
-window.fbAsyncInit = function () {
-  FB.init({
-    appId: "591873306363263",
-    autoLogAppEvents: true,
-    xfbml: true,
-    version: "v13.0"
-  });
-};
+// window.fbAsyncInit = function () {
+//   FB.init({
+//     appId: "591873306363263",
+//     autoLogAppEvents: true,
+//     xfbml: true,
+//     version: "v13.0"
+//   });
+// };
 
-(function (d, s, id) {
-  var js,
-    fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s);
-  js.id = id;
-  js.src = "https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js";
-  fjs.parentNode.insertBefore(js, fjs);
-})(document, "script", "facebook-jssdk");
+// (function (d, s, id) {
+//   var js,
+//     fjs = d.getElementsByTagName(s)[0];
+//   if (d.getElementById(id)) return;
+//   js = d.createElement(s);
+//   js.id = id;
+//   js.src = "https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js";
+//   fjs.parentNode.insertBefore(js, fjs);
+// })(document, "script", "facebook-jssdk");
+function loadFacebookChat() {
+  // Load the Facebook SDK asynchronously
+  window.fbAsyncInit = function () {
+    FB.init({
+      appId: "591873306363263",
+      autoLogAppEvents: true,
+      xfbml: true,
+      version: "v13.0"
+    });
+
+    // Initialize the Facebook chat plugin
+    FB.CustomerChat.showDialog();
+  };
+
+  (function (d, s, id) {
+    var js,
+      fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s);
+    js.id = id;
+    js.src = "https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js";
+    fjs.parentNode.insertBefore(js, fjs);
+  })(document, "script", "facebook-jssdk");
+}
+
+function loadFacebookChatLazy() {
+  if (window.requestIdleCallback) {
+    requestIdleCallback(function () {
+      loadFacebookChat();
+    });
+  } else {
+    setTimeout(function () {
+      loadFacebookChat();
+    }, 500);
+  }
+}
+
+// Trigger the lazy loading of the Facebook chat plugin when the window finishes loading
+window.addEventListener("load", loadFacebookChatLazy);
 
 /****************end Official FACEBOOK SDK****************** */
 
--- assets/js/script.js
+++ assets/js/script.js
@@ -31,41 +31,23 @@ sections.forEach(e => {
 
 /***************************Facebook live chat************ */
 /****************Official FACEBOOK SDK****************** */
-window.fbAsyncInit = function () {
-  FB.init({
-    appId: "591873306363263",
-    autoLogAppEvents: true,
-    xfbml: true,
-    version: "v13.0"
-  });
-};
+// window.fbAsyncInit = function () {
+//   FB.init({
+//     appId: "591873306363263",
+//     autoLogAppEvents: true,
+//     xfbml: true,
+//     version: "v13.0"
+//   });
+// };
 
-(function (d, s, id) {
-  var js,
-    fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s);
-  js.id = id;
-  js.src = "https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js";
-  fjs.parentNode.insertBefore(js, fjs);
-})(document, "script", "facebook-jssdk");
+// (function (d, s, id) {
+//   var js,
+//     fjs = d.getElementsByTagName(s)[0];
+//   if (d.getElementById(id)) return;
+//   js = d.createElement(s);
+//   js.id = id;
+//   js.src = "https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js";
+//   fjs.parentNode.insertBefore(js, fjs);
+// })(document, "script", "facebook-jssdk");
 
-/****************end Official FACEBOOK SDK****************** */
-// function chatboxInit() {
-//   window.fbAsyncInit = function () {
-//     FB.init({
-//       xfbml: true,
-//       version: "v14.0"
-//     });
-//   };
-//   (function (d, s, id) {
-//     var js,
-//       fjs = d.getElementsByTagName(s)[0];
-//     if (d.getElementById(id)) return;
-//     js = d.createElement(s);
-//     js.id = id;
-//     js.src = "https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js";
-//     fjs.parentNode.insertBefore(js, fjs);
-//   })(document, "script", "facebook-jssdk");
-// }
-// chatboxInit();
+/*******************End Customize FACEBOOK SDK****************** */
